a1b8aeb07ebe0504bf557e25df96bdd63601d5cf
mongolrgata <mongolrgata@gmail.com>
2015-02-18 11:47:59 +0300

    Add page action for nyaa.se

--- background.js
+++ background.js
@@ -88,3 +88,23 @@ chrome.runtime.onInstalled.addListener(function () {
 chrome.runtime.onStartup.addListener(function () {
     setAlarm();
 });
+
+// TODO Some magic here... i dunno lol
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions : [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl : {urlPrefix : 'http://www.nyaa.se'}
+                    })
+                ],
+                actions    : [new chrome.declarativeContent.ShowPageAction()]
+            }
+        ]);
+    });
+});
+
+chrome.pageAction.onClicked.addListener(function () {
+    alert('TODO');
+});
