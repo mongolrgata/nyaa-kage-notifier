053a9f0bb37a0bf72946216c9a694e7d644c471d
mongolrgata <mongolrgata@gmail.com>
2015-04-15 17:07:26 +0300

    All storage

--- background.js
+++ background.js
@@ -15,4 +15,33 @@ chrome.runtime.onInstalled.addListener(function () {
    });
 });
 
-alert($.fn.jquery);
+chrome.pageAction.onClicked.addListener(function (tab) {
+   var lol = tab.url;
+
+   chrome.tabs.executeScript(
+         tab.id,
+         {
+            code : "                                                              " +
+            "(function(){                                                         " +
+            "  return $('body').find(\".tlist .tlistrow:first .tlistname\").text();" +
+            "})();                                                                 "
+         },
+         function (result) {
+            var opop = result[0];
+
+            chrome.storage.local.get(null, function (res) {
+               if (!res.hasOwnProperty('nyaa')) {
+                  res.nyaa = {};
+               }
+
+               res.nyaa[lol] = {
+                  topStack : opop,
+                  aname    : opop.match(/(\[.*?])?(.*?)(\[|\.|-)/)[2].trim(),
+                  trans    : opop.match(/\[(.*?)]/)[1]
+               };
+
+               chrome.storage.local.set({nyaa : res.nyaa});
+            });
+         }
+   );
+});
--- myscript.js
+++ myscript.js
@@ -1,7 +1,30 @@
-(function() {
+(function () {
    var $button = $('input[alt="Скачать"]');
-   var newWatchElem = $('<img/>').attr('src', chrome.extension.getURL('images/38.png')).css('height', 20);
-   $button.each(function(index, element) {
-      $(element).after(newWatchElem.clone());
+   var aname = $($('tr:contains("Альтернативные названия:")')[1]).parent().find('tr:first').text().trim();
+   var newWatchElem = $('<img/>').attr('src', chrome.extension.getURL('images/38.png')).css('height', 20).addClass('ololo');
+   $button.each(function (index, element) {
+      var $form = $(element).closest('table').find('form');
+      var url = window.location.protocol + "//" + window.location.host + "/" + $form.attr("action") + "?" + $form.serialize();
+      $(element).after(newWatchElem.clone().data('ololo', url));
+   });
+
+   $('.ololo').click(function () {
+      var $img = $(this);
+      var opop = $img.parent().next().text();
+      var trans = $($img.closest('table').next().find('[href]')[0]).text().trim();
+
+      chrome.storage.local.get(null, function (res) {
+         if (!res.hasOwnProperty('kage')) {
+            res.kage = {};
+         }
+
+         res.kage[$img.data('ololo')] = {
+            topStack: opop,
+            aname: aname,
+            trans: trans
+         };
+
+         chrome.storage.local.set({kage : res.kage});
+      });
    });
 })();
